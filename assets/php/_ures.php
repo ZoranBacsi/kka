df3e205a1f4eef84eddbc8f5e31e8a8f6f5e50be
cserlac <csernyilaszlo.kassaiter@gmail.com>
2017-02-02 13:01:24 +0100

    -

---
+++ assets/php/_ures.php
@@ -0,0 +1,26 @@
+<?php
+// echo "<HTML><HEAD><meta http-equiv='Content-Type' content='text/html;charset=ISO-8859-1'/><link rel='stylesheet' type='text/css' href='assets/css/main.css' /></HEAD>";
+echo "<HTML><HEAD>";
+echo "<meta charset='utf-8'>";
+echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+echo "<link rel='icon' href='assets/img/favicon.ico' type='image/ico' sizes='16x16'>";
+echo "<link type='text/css' rel='stylesheet' media='all' href='assets/css/bootstrap.min.css'>";
+echo "<link type='text/css' rel='stylesheet' media='all' href='assets/css/bootstrap-theme.min.css'>";
+echo "<link type='text/css' rel='stylesheet' media='all' href='assets/css/main.css'>";
+echo "<script src='assets/js/jquery-3.1.1.min.js'></script>";
+echo "<script src='assets/js/bootstrap.min.js'></script>";
+echo "<script src='assets/js/api.js'></script>";
+echo "</HEAD>";
+
+
+
+echo "<body>";
+require("a_kapcs.inc.cS7.php");
+dbkapcs();
+
+
+
+
+echo "</body></HTML>";
+?>
+
